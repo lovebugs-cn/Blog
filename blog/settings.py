d61a5047e6b426681ee8549a859d8d219f63e3de
Josiah <1319164238@qq.com>
2017-09-19 18:01:38 +0800

    fourthchange

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7ka8ed*3q22d)pz@#0c=#gqo41bfx#h@5k8pwh&%q4g187-n28'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','.linyachao.com']
 
